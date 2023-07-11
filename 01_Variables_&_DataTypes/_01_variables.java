6762ecac2a4d22d9dc2512533a0de8e209189374
harshyadav02 <hy043768@gmail.com>
2023-07-11 17:39:53 +0530

    Variables in java

---
+++ 01_Variables_&_DataTypes/_01_variables.java
@@ -0,0 +1,42 @@
+// Variables in java 
+
+ class _01_variables{
+	public static void main(String args[]){
+		// byte range is -128 to 127 an 1 byte is equal to 8 bit
+		byte b = 111;
+
+		// Short range is -32768 to -32767
+		short s = 23;
+		
+		// int range is -2147483648 to 2147483647
+		int i = 10;
+
+		// float range is upto 7 decimal
+		float f = 22f;
+
+		// double range is upto 16 decimal value
+		double d = 24;
+
+
+		String S = "Harsh Yadav";
+
+		// Char range is 0 to 255
+		char  c = 'a';
+
+		// Boolean contain two values true and false (by default false)
+		boolean bol = true; 
+
+
+
+		System.out.println(s);
+		System.out.println(b);
+		System.out.println(i);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(S);
+		System.out.println(c);
+		System.out.println(bol);
+
+
+	}
+}
