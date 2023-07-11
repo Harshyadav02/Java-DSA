c89f833b5dab55b15662fe362c854c5a2371d54b
harshyadav02 <hy043768@gmail.com>
2023-07-11 17:45:36 +0530

    Taking a Single character as Input from user

---
+++ 01_Variables_&_DataTypes/_08_charAt.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class _09_charAt{
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		// chatAt(0) is used to take a single character 
+		char c = sc.next().charAt(0);
+
+		System.out.println(c);
+	}
+}
