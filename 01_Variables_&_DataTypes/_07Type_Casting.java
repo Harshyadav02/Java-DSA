d80d07ad93b1da8f115a89c53da5981ef4b73798
harshyadav02 <hy043768@gmail.com>
2023-07-11 17:44:30 +0530

    Type Casting in java

---
+++ 01_Variables_&_DataTypes/_07Type_Casting.java
@@ -0,0 +1,14 @@
+class _07Type_Casting{
+	public static void main(String args[]){
+		float x = 99.99f;
+		// converting float value to integer
+		int y = (int)x;
+
+
+		// converting int value to float
+		int z = 10;
+		float a = (float)z;
+		System.out.println(y);
+		System.out.println(a);
+	}
+}
