c7d25d0f871cdf83792f08497f3e5b2600a46cb1
harshyadav02 <hy043768@gmail.com>
2023-07-12 20:50:54 +0530

    = ,+= ,-= ,*= ,/=

---
+++ 02_Operators/_05Assignment_Operators.java
@@ -0,0 +1,42 @@
+class _05Assignment_Operators{
+	public static void main(String[] args) {
+		
+		/* 
+			Assignment Operators
+
+
+			=  
+			+=
+			-=
+			*=
+			/=
+
+
+
+
+		*/
+
+		// Use of  = Operator
+
+			int a  = 10;
+
+		// use of += Operator
+
+			a += 1; // output 11
+
+		// use of -= Operator
+
+			int  b = 10;
+
+			b -= 1; // output 9
+
+		// use of *= Operator
+
+			a *= 2; //output 22
+
+		// use of /= Operator
+
+			b  /= 2 ; //output 4
+
+	}
+}
