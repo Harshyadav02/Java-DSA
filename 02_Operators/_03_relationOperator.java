763322cd13b0d1c31373c1aef937e7f60f7c3549
harshyadav02 <hy043768@gmail.com>
2023-07-12 20:48:06 +0530

    == ,!= ,> ,<,>= <=

---
+++ 02_Operators/_03_relationOperator.java
@@ -0,0 +1,27 @@
+class _03_relationOperator{
+	public static void main(String[] args) {
+		
+		// Relation Operators
+
+		/* 
+			== (equal Operator) used for comparsion and return true/false
+			!= (Not equal to) return true/false
+			>  (Greater then) return true/false
+			<  (Less Then)	  return true/false
+
+			>= (Greater Then and equal to) return true/false
+			<= (Less Then and Equal to)    return true/false
+		*/
+
+		int x = 10;
+		int y = 20;
+
+		System.out.println("Using == operator : "+ (x == y));
+		System.out.println("Using != operator : "+ (x != y));
+		System.out.println("Using > operator : "+  (x >  y));
+		System.out.println("Using < operator : "+  (x <  y));
+		System.out.println("Using >= operator : "+ (x >= y));
+		System.out.println("Using =< operator : "+ (x <= y));
+
+	}
+}
