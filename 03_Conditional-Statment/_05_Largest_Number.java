ade1d88d5f09effbb373d46ba39fe3b4d2beb7d4
harshyadav02 <hy043768@gmail.com>
2023-07-14 20:26:01 +0530

    Largest Number among 3 Number

---
+++ 03_Conditional-Statment/_05_Largest_Number.java
@@ -0,0 +1,19 @@
+class _05_Largest_Number{
+	public static void main(String[] args) {
+		
+		int x = 10 , y = 20 ,z = 30;
+		
+
+		if (x > y && x > z){
+			System.out.println("x is Greater Number ");
+		}
+
+		else if(y > x && y > z){
+			System.out.println("y is greater ");
+		}
+
+		else{
+			System.out.println("z is greater ");
+		}
+	}
+}
