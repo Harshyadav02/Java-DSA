5cc06b37c3b0ccb1734e2f190421142a1946d064
harshyadav02 <hy043768@gmail.com>
2023-07-14 20:26:38 +0530

    Tarnary operator

---
+++ 03_Conditional-Statment/_06_TernaryOperator.java
@@ -0,0 +1,20 @@
+class _06_TernaryOperator{
+
+	public static void main(String[] args) {
+		// Ternary Operator is used for condition
+
+		/* 
+			Syntex :- 
+				variable = condition ? statment1: statment2;
+
+				if condition is true then statment1 will execute else statmt 2 will execute
+		*/	
+
+		int x = 10;
+		int y = 20;
+
+		String s = (10 > 20)?"10 is greater" : "20 is Greater";
+
+		System.out.println(s);
+	}
+}
