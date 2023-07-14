5684e83a914235905a6048452dbbf91dc5bbe80f
harshyadav02 <hy043768@gmail.com>
2023-07-14 20:20:44 +0530

    If -else

---
+++ 03_Conditional-Statment/_01_If_else.java
@@ -0,0 +1,30 @@
+class _01_If_else{
+	public static void main(String args[]){
+
+		// If - Else are used for condition 
+
+		/* 
+
+			If the condition is true then the if block will execute , otherwise else block will execute
+
+			Note :- we can write multiple if , but there will be only else
+		*/
+
+		int x = 10;
+		
+		// In brackets we write condition
+		if(x > 5){
+			System.out.println("X is greater");
+		}
+		
+		if (x == 5) {
+			System.out.println("X is eqaul to 5");
+		}
+		else{
+			System.out.println("X is smaller");
+		}
+
+		// System.out.println(y);
+		System.out.println();
+	}
+}
