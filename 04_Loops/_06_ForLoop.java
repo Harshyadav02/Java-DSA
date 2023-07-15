aae3d69b6f29edff18fb5dcc1836828f27c78181
harshyadav02 <hy043768@gmail.com>
2023-07-15 13:51:48 +0530

    For loop

---
+++ 04_Loops/_06_ForLoop.java
@@ -0,0 +1,18 @@
+class _06_ForLoop{
+	public static void main(String[] args) {
+		// For Loop
+
+		/*
+			syntex 
+			for(initialistion ; condition ; updation){
+				# work
+			}
+		*/
+
+		// Printing number upto 10 by for loop
+
+			for(int i= 1; i<=10; i++){
+				System.out.print(i + " ");
+			}
+	}
+}
