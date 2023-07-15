8f53390b2a1f130cfe70ce5ceac0924588c447e2
harshyadav02 <hy043768@gmail.com>
2023-07-15 13:56:28 +0530

    DO While loop

---
+++ 04_Loops/_09_DoWhile.java
@@ -0,0 +1,23 @@
+class _09_D0While{
+	public static void main(String[] args) {
+		
+		// Do Wile 
+
+		/* 
+			Do{
+				
+				code
+			}while(condation);
+
+			Note:- Do while will run once wheater the condition is satisfied or not .
+		*/
+
+		int x = 1;
+
+		do{
+			System.out.println("Hello Anna ");
+			x +=1;
+		}while(x < 10);
+
+	}
+}
