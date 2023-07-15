01c2f84c8cf48c86c1f0fbc61aac6d55530c08c8
harshyadav02 <hy043768@gmail.com>
2023-07-15 13:49:06 +0530

    While loop is used when the ending itreation is unknown

---
+++ 04_Loops/_01_WhileLoop.java
@@ -0,0 +1,21 @@
+class _01_whileLoop{
+	public static void main(String[] args) {
+		// While loop
+
+		/*
+			While(condition){
+				# code 
+			}
+
+			while loop will run until the condition is false
+		*/
+
+		int x = 0;
+
+		while(x < 10){
+			System.out.println("Hello world");
+			x++;
+		}
+
+	}
+}
