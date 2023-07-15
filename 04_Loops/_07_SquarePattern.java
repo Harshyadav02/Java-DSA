8042836deb10f6dfd7476d92ccb895ebadd3d40a
harshyadav02 <hy043768@gmail.com>
2023-07-15 13:55:13 +0530

    Square patteren using nested for loop

---
+++ 04_Loops/_07_SquarePattern.java
@@ -0,0 +1,12 @@
+class _07_SquarePattern{
+	public static void main(String[] args) {
+
+		// Printing a Square of * (4*4)
+		for(int i = 0 ; i<4; i++){
+			for(int j=0 ; j<4; j++){
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+	}
+}
