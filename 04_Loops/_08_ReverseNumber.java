4149f93ceadbd8be814ebfe604d1d4e062a30ccf
harshyadav02 <hy043768@gmail.com>
2023-07-15 13:55:39 +0530

    Reversing a Number

---
+++ 04_Loops/_08_ReverseNumber.java
@@ -0,0 +1,14 @@
+class _08_ReverseNumber{
+	public static void main(String[] args) {
+		int x = 233;
+
+		
+		while(x > 0){
+			// to achive last digit of a number use num%10
+			System.out.print(x%10);
+
+			// to remove last digit of a number use num/10
+			x = x/10;
+		}
+	}
+}
