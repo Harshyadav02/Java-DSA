c5558e1531a80dcff71e25dc588d0c3fc230167b
harshyadav02 <hy043768@gmail.com>
2023-07-15 13:57:01 +0530

    Break is used to break the loop

---
+++ 04_Loops/_10_Break.java
@@ -0,0 +1,15 @@
+class _10_Break{
+
+	public static void main(String[] args) {
+		//  Break is to the exit the loop 
+
+		for(int i=0; i<=10 ; i++ ){
+
+			if(i == 5) break;
+
+			else{
+				System.out.println("Hello world ");
+			}
+		}
+	}
+}
