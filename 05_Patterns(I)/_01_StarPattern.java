189d913b3dff064be7924b6a875384542f9b7831
harshyadav02 <hy043768@gmail.com>
2023-07-16 18:47:14 +0530

    Start Pattern

---
+++ 05_Patterns(I)/_01_StarPattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class _01_StarPattern{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+
+		// Way 1 to print star 
+		for(int i=0 ;  i<x ; i++){
+			for(int j = 0; j<=i; j++){
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+/*---------------------------------------------------------------------------------------------------------*/
+
+		// Way 2 to print star
+
+		for(int i=1 ;  i<=x ; i++){
+				System.out.println("*".repeat(i));
+
+			
+		}
+	}
+}
