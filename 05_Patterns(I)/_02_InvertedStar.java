5bf01a10a2d335e8d854457189002b852b14d756
harshyadav02 <hy043768@gmail.com>
2023-07-16 18:55:34 +0530

    Inverted Star

---
+++ 05_Patterns(I)/_02_InvertedStar.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class _02_InvertedStar{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+
+		// Way 1 to print star 
+		for(int i=x ;  i>=1 ; i--){
+			for(int j = 1; j<=i; j++){
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+/*---------------------------------------------------------------------------------------------------------*/
+
+		// Way 2 to print star
+
+		for(int i=x ;  i>=1 ; i--){
+				System.out.println("*".repeat(i));
+
+			
+		}
+	}
+}
