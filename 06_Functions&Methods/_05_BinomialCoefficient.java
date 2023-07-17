879f6e3a0c41b65e2dc7794fbc9280be394debe2
harshyadav02 <hy043768@gmail.com>
2023-07-17 20:17:05 +0530

    Combination

---
+++ 06_Functions&Methods/_05_BinomialCoefficient.java
@@ -0,0 +1,28 @@
+class _05_BinomialCoefficient{
+
+	public static int BinomialCoefficient(int n , int r ,int nmr){
+		int Coff = n/(r*nmr);
+			return Coff;
+	}
+
+	public static int factorial(int num){
+		int fact = 1;
+		for(int i = 1 ;i<=num; i++){
+
+			fact = fact * i;
+		}
+		return fact;
+	}
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		int r = 2;
+		int fact_n = factorial(n);
+		int fact_r = factorial(r);
+		int nmr = factorial(n-r); 
+		System.out.println(BinomialCoefficient(fact_n,fact_r,nmr));
+
+	}
+
+}
