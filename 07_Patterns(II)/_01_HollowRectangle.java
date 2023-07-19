5b9c749efde642faf9766cb13d43f4fad0e6426e
harshyadav02 <hy043768@gmail.com>
2023-07-19 19:17:27 +0530

    [200~* * * *

---
+++ 07_Patterns(II)/_01_HollowRectangle.java
@@ -0,0 +1,45 @@
+/*
+print Hollow Rectangle Tringle 
+
+	* * * *
+	*     *
+	*     *
+	*     *
+	* * * * 
+
+
+*/
+
+import java.util.Scanner ;
+class _01_HollowRectangle{
+
+	public static void pattern(int rows , int cols){
+
+		// for rows
+		for(int i = 1 ; i <=rows; i++){
+			// for cols
+			for(int j = 1; j <=cols; j++){
+				
+				// cells (i,j)
+				if(i == 1 || i == rows  || j == 1 || j == cols){
+					// Boundary cells
+					System.out.print("* ");
+				}
+				// Inner cells
+				else System.out.print("  ");
+			}
+			System.out.println();
+	}
+}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the Number of Rows : ");
+		int rows = sc.nextInt();
+		
+		System.out.print("Enter the Number of Cols : ");
+		int cols = sc.nextInt();
+		
+		pattern(rows,cols);
+	}
+}
