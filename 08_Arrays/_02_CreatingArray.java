8fa7da218e028f48c8380d7e46d95df01092b8bf
harshyadav02 <hy043768@gmail.com>
2023-07-21 21:34:33 +0530

    Creation of arrays

---
+++ 08_Arrays/_02_CreatingArray.java
@@ -0,0 +1,18 @@
+class _02_CreatingArray{
+
+	/*
+	Syntex :-
+		dataType arrayName[] = new dataType [size];
+	
+	*/
+
+	// creating a array
+	int marks[] = new int[5];
+
+	// creating and intilizing an array
+
+	int number[] = {1,2,4,5,6};
+
+	String fruits[] = new String[2];
+
+}
