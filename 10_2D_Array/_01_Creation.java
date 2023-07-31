967c1c64b7fe05623adba137e776c0b721d6b54f
harshyadav02 <hy043768@gmail.com>
2023-07-31 21:38:46 +0530

    Creating two dimensional array

---
+++ 10_2D_Array/_01_Creation.java
@@ -0,0 +1,18 @@
+/* 2D arrays are also known as matrix the hold the data in form of rows , coloumn  .
+
+Indxing of 2D-Array starts from 0 
+
+*/
+
+
+class _01_Creation{
+
+	public static void main(String[] args) {
+		
+		//  creation of 2D-Array 
+
+		int arr[][] = new int[4][4];
+
+		// above we have created 4 X 4 two-dimension array, Total number of cells will be 16 
+	}
+}
