61df6dc6a83b187ac4c658184a4bad79e5e78968
harshyadav02 <hy043768@gmail.com>
2023-07-31 21:37:03 +0530

    Insertion sort

---
+++ 09_Basic-Sorting-Algo/_03_Insertion_sort.java
@@ -0,0 +1,34 @@
+public class _03_Insertion_sort {
+
+	public static int[] sort(int arr[]){
+
+		for(int i=1; i<arr.length;i++){
+
+			int current_element = arr[i];
+			int prev_index = i-1;
+
+			while(prev_index >=0 && arr[prev_index] > current_element){
+
+				arr[prev_index+1] = arr[prev_index];
+				prev_index --;
+			}
+
+			arr[prev_index+1] = current_element;	
+		
+		}
+
+		return arr;
+
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {5,4,3,2,1};
+
+		sort(arr);
+
+		for(int array : arr){
+			System.out.print(array + " ");
+		}
+	}
+
+}
