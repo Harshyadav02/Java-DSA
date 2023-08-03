e2f0628242c95b6378a9f1fc817c9604ac013785
harshyadav02 <hy043768@gmail.com>
2023-08-03 20:47:38 +0530

    Char At Method gives letter at a particular index

---
+++ 11_Strings/_05_charAt.java
@@ -0,0 +1,17 @@
+class _05_charAt{
+
+	public static void main(String[] args) {
+		
+		String first_name = "Harsh";
+		String last_name = "Yadav";
+
+		
+		String Full_Name = first_name + " " + last_name;
+
+		//  charAt() method return the letter at particular index , charAt means Charcater At index
+		System.out.println(Full_Name.charAt(0));
+
+
+
+	}
+}
