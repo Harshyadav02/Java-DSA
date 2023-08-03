cfc28ed8dc667965dfa608e6ef6492653470fdc1
harshyadav02 <hy043768@gmail.com>
2023-08-03 20:45:53 +0530

    length of string

---
+++ 11_Strings/_03_StringLength.java
@@ -0,0 +1,17 @@
+class _03_StringLength{
+
+	public static void main(String[] args) {
+		
+		String str = "Harsh yadav";
+
+		// Length of a string 
+
+		System.out.println(str.length());  				// Output :- 11
+	}
+}
+
+/*
+
+	Note:- here length takes () and white space is also considered as string 
+*/
+
