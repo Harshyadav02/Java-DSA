979bc11613afaf634e2f0d68a91e52fa6cb55a70
harshyadav02 <hy043768@gmail.com>
2023-08-03 20:46:40 +0530

    String Concatenation

---
+++ 11_Strings/_04_Sting_Concatenation.java
@@ -0,0 +1,13 @@
+class _04_Sting_Concatenation{
+
+	public static void main(String[] args) {
+		
+		String first_name = "Harsh";
+		String last_name = "Yadav";
+
+		//  String Concatenation
+		String Full_Name = first_name + " " + last_name;
+		System.out.println(Full_Name);
+
+	}
+}
