06fb178104e0a3db8476ffd154dea4bac281c442
harshyadav02 <hy043768@gmail.com>
2023-08-03 20:43:45 +0530

    Creating string

---
+++ 11_Strings/_01_StringCreation.java
@@ -0,0 +1,19 @@
+class _01_StringCreation{
+
+	public static void main(String[] args) {
+		
+		// String Creation way-1
+
+		String str = "Harsh Yadav is a Hard core Programer"
+
+
+		// String Creation way-2
+
+		String str2  = new String("Harsh Yadav is vey good Programer")
+	}
+}
+
+/*Note :- 
+		Java Strings are immutable
+
+Both way differ at implemention level not at program level */
