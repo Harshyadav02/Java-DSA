0ae598e5ee837c6d9a84548f7f52fc84ef71d655
harshyadav02 <hy043768@gmail.com>
2023-08-05 19:42:10 +0530

    Taking input in java

--- 01_Variables_&_DataTypes/_04_Input_in_Java.java
+++ 01_Variables_&_DataTypes/_04_Input_in_Java.java
@@ -8,34 +8,34 @@ class _04_Input_in_Java{
 		// Created an object of Scanner class an sc is a reference variable ,System.in refer to input
 		Scanner sc  = new Scanner(System.in);
 		
-		System.out.print("Enter the value of x: ");
+		System.out.print("Enter the integer value : ");
 		// nextInt used to take integer input
-		int x = sc.nextInt();
+		int integer_input = sc.nextInt();
 
 		System.out.print("Enter the value of f: ");
 		// nextFloat used to take Float input
-		float f = sc.nextFloat();
+		float float_input= sc.nextFloat();
 
 		System.out.print("Enter the value of d: ");
 		// nextDouble used to take Double input
-		double d = sc.nextDouble();
+		double double_input = sc.nextDouble();
 
 		System.out.print("Enter the value of s: ");
 		// next used to take a single word input
-		String s = sc.next();
+		String single_word_input = sc.next();
 		
 
 		System.out.print("Enter the value of sl: ");
 		// nextLine used to take sting (a sentance) as input
-		String sl = sc.nextLine();
+		String string_line_input = sc.nextLine();
 
 		System.out.println();
 		System.out.println("The output are below: ");
-		System.out.println(x);
-		System.out.println(f);
-		System.out.println(d);
-		System.out.println(s);
-		System.out.println(sl);
+		System.out.println(integer_input);
+		System.out.println(float_input);
+		System.out.println(double_input);
+		System.out.println(single_word_input);
+		System.out.println(string_line_input);
 		
 		sc.close();
 
