33cb90d3e4d29d9bd8c2692971ca764be02a8862
harshyadav02 <hy043768@gmail.com>
2023-08-05 19:35:24 +0530

    Sum of two number in java

---
+++ 01_Variables_&_DataTypes/_02_SumOfTwoNumber.java
@@ -0,0 +1,10 @@
+class _02_SumOfTwoNumber{
+	public static void main(String args[]){
+		int first_number = 10;
+		int second_number = 20;
+
+		System.out.println(first_number+second_number);
+
+		
+	}
+}
