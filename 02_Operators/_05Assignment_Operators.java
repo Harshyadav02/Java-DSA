7050b461ebac0134cdcaec77ffd1366d6e8652b7
harshyadav02 <hy043768@gmail.com>
2023-08-05 20:06:31 +0530

    Assignment Operator

--- 02_Operators/_05Assignment_Operators.java
+++ 02_Operators/_05Assignment_Operators.java
@@ -18,25 +18,25 @@ class _05Assignment_Operators{
 
 		// Use of  = Operator
 
-			int a  = 10;
+			int value1  = 10;
 
 		// use of += Operator
 
-			a += 1; // output 11
+			value1 += 1; // output 11
 
 		// use of -= Operator
 
-			int  b = 10;
+			int  value2 = 10;
 
-			b -= 1; // output 9
+			value2 -= 1; // output 9
 
 		// use of *= Operator
 
-			a *= 2; //output 22
+			value1 *= 2; //output 22
 
 		// use of /= Operator
 
-			b  /= 2 ; //output 4
+			value2  /= 2 ; //output 4
 
 	}
 }
