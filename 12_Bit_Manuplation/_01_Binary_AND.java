ca0a7885498dabf82654a6a7de9366a76d6d7b4a
harshyadav02 <hy043768@gmail.com>
2023-08-07 22:19:15 +0530

    About Binary AND

---
+++ 12_Bit_Manuplation/_01_Binary_AND.java
@@ -0,0 +1,28 @@
+public class _01_Binary_AND{
+	/*
+
+	0 & 0 ---> 0
+
+	0 & 1 ---> 0
+
+	1 & 0 ---> 0
+
+	1 & 1 ---> 1
+
+	 For example :- 
+
+	  5 & 6 ---> 4
+
+	  5 --> 101(Binary of 5)
+	  6 --> 110(Binary of 6)
+
+	   101
+	 & 110
+	--------
+	   100 ---> 4 
+	*/
+	public static void main(String[] args) {
+		
+		System.out.println(5 & 6);   // Output 4
+	}
+}
