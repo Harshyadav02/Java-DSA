efd6cd7623ff3da61a7f4c3ce0e88e86812ceb81
harshyadav02 <hy043768@gmail.com>
2023-08-07 22:23:57 +0530

    Binary OR

---
+++ 12_Bit_Manuplation/_02_Binary_OR.java
@@ -0,0 +1,29 @@
+class _02_Binary_OR{
+
+	/*
+
+	0 & 0 ---> 0
+
+	0 & 1 ---> 1
+
+	1 & 0 ---> 1
+
+	1 & 1 ---> 1
+
+	 For example :- 
+
+	  5 & 6 ---> 4
+
+	  5 --> 101(Binary of 5)
+	  6 --> 110(Binary of 6)
+
+	   101
+	 & 110
+	--------
+	   111 ---> 7 
+	*/
+	public static void main(String[] args) {
+		
+		System.out.println(5 | 6);   // Output 7
+	}
+}
