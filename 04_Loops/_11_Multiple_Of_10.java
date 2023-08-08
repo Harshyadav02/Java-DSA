bbf8906b8aa1aab18668cfed590719b16957bff8
Harsh Yadav <91467622+Harshyadav02@users.noreply.github.com>
2023-08-08 18:08:08 +0530

    Break the loop if user enter multiple of 10

--- 04_Loops/_11_Multiple_Of_10.java
+++ 04_Loops/_11_Multiple_Of_10.java
@@ -1,5 +1,6 @@
+/*Break the loop if user input multiple of 10*/
 import java.util.Scanner;
-class _11_Multiple_10{
+class _11_Multiple_Of_10{
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
