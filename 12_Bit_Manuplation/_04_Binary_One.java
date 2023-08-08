cb73fdfd4ae33d9e753c95c754bfa0b6c48f3ba2
harshyadav02 <hy043768@gmail.com>
2023-08-08 16:26:46 +0530

    Binary One's

---
+++ 12_Bit_Manuplation/_04_Binary_One.java
@@ -0,0 +1,20 @@
+class _04_Binary_One{
+
+		/*
+	~ works on single operand
+
+	~ 0  ---> 1
+	~ 1  ---> 0
+
+ 	for example:- 
+ 		~5 
+
+ 		~101 --> 010 -->(-6)
+
+	
+	*/
+	public static void main(String[] args) {
+		
+		System.out.println(~5);   // Output -6
+	}
+}
