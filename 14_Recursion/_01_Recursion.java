409361013272f209cda9f99276c1d0722122d311
harshyadav02 <hy043768@gmail.com>
2023-08-26 22:05:17 +0530

    Recursion in java

---
+++ 14_Recursion/_01_Recursion.java
@@ -0,0 +1,24 @@
+class _01_Recursion{
+
+
+	public static void main(String[] args) {
+		
+		/*
+
+		if a problem is described in term of problem but in reduced manner with an terminating condition is called recursion
+
+
+		And a function calling it self with an terminating condition is called recursive function
+
+
+	*/
+
+		// Three things to do in recusrion 
+
+			// 1) Define Base case  (MANDATORY)
+			// 2) Work to do
+
+			// 3) Calling inner function
+
+	}
+}
