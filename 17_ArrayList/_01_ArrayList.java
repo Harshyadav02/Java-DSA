f436241aaa50804c2a080c13ceec201183e206af
harshyadav02 <hy043768@gmail.com>
2023-09-05 22:07:59 +0530

    About ArrayList

---
+++ 17_ArrayList/_01_ArrayList.java
@@ -0,0 +1,34 @@
+// Importinf ArrayList in java
+
+import java.util.ArrayList;
+
+class _01_ArrayList{
+
+	/*  Array list is an inbuilt data structure in java
+
+	which are dynamic in nature and store Object type (Not primitive data type)  
+
+	ArrayList is part Java Collection framewok
+	*/
+
+
+	public static void main(String[] args) {
+		
+		// Creatiion of arraylist which will hold Interger type data
+
+		ArrayList <Integer> list = new ArrayList<>();
+		
+		// arraylist which will hold String type data
+		ArrayList <String> Str_list = new ArrayList<>();
+		
+		// arraylist which will hold Boolean type data
+		ArrayList <Boolean> Bool_list = new ArrayList<>();
+		
+		//arraylist which will hold Double type data
+		ArrayList <Double> Double_list = new ArrayList<>();
+		
+		// arraylist which will hold Float type data
+		ArrayList <Float> Float_list = new ArrayList<>();
+	}
+
+}
