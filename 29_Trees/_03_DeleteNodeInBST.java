ae3c865f57190e9fb5d8a4d4f11d39b46f2cc5b0
Harshyadav02 <harshyadav5736@gmail.com>
2024-05-30 09:02:37 +0530

    Deleting a node in BST

---
+++ 29_Trees/_03_DeleteNodeInBST.java
@@ -0,0 +1,35 @@
+class _03_DeleteNodeInBST {
+    public TreeNode deleteNode(TreeNode root, int key) {
+    // searching process
+    if (root == null) return root;
+    
+    if (root.val < key) {
+        root.right = deleteNode(root.right, key);
+    } else if (root.val > key) {
+        root.left = deleteNode(root.left, key);
+    } else {
+        // if found
+        // leaf Node or single child
+        if (root.left == null) {
+            return root.right;
+        } else if (root.right == null) {
+            return root.left;
+        }
+        
+        // two children
+        TreeNode IS = findInorderSuccessor(root.right);
+        root.val = IS.val;
+        root.right = deleteNode(root.right, IS.val);
+    }
+
+    return root;
+}
+
+    private TreeNode findInorderSuccessor(TreeNode root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+}
